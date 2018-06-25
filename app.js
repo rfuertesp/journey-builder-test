801e71d6493d88edfada3740ebaf55f356183433
ES\rfuertes <rfuertes@deloitte.es>
2018-06-25 17:54:47 +0200

    no message

---
+++ app.js
@@ -0,0 +1,18 @@
+'use strict';
+// Module Dependencies
+// -------------------
+var express = require('express');
+var app = express();
+
+var app = express();
+
+app.get('/', function (req, res) {
+  res.send('Hello World!');
+});
+
+// Configure Express
+app.set('port', process.env.PORT || 3000);
+
+http.createServer(app).listen(app.get('port'), function(){
+  console.log('Express server listening on port ' + app.get('port'));
+});

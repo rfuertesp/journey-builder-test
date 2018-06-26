be3cbd770eea31312e2ac01af09ef20648a29bd8
ES\rfuertes <rfuertes@deloitte.es>
2018-06-26 17:19:20 +0200

    no message

--- app.js
+++ app.js
@@ -1,22 +1,38 @@
 'use strict';
 // Module Dependencies
 // -------------------
-var express = require('express');
-var http = require('http');
-var path = require('path');
-var app = express();
+var express     = require('express');
+var bodyParser  = require('body-parser');
+var errorhandler = require('errorhandler');
+var http        = require('http');
+var path        = require('path');
+var request     = require('request');
+var routes      = require('./routes');
+var activity    = require('./routes/activity');
 
 var app = express();
-
-
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.get('/', function (req, res) {
-  res.send('Hello World!');
-});
 
 // Configure Express
 app.set('port', process.env.PORT || 3000);
+app.use(bodyParser.raw({type: 'application/jwt'}));
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Express in Development Mode
+if ('development' == app.get('env')) {
+  app.use(errorhandler());
+}
+
+// HubExchange Routes
+app.get('/', routes.index );
+app.post('/login', routes.login );
+app.post('/logout', routes.logout );
+
+// Custom Hello World Activity Routes
+app.post('/save/', activity.save );
+app.post('/validate/', activity.validate );
+app.post('/publish/', activity.publish );
+app.post('/execute/', activity.execute );
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
